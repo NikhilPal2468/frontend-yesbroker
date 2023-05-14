89449c0400f0cdcbe9c6eac876e279590b832a83
NikhilPal2468 <nikhilpal2468@gmail.com>
2023-05-14 13:00:07 +0530

    authentication started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import { useState } from 'react';
 import './App.css';
+import Authentication from './components/Authentication';
 
 function App() {
+  const [show, setShow] = useState(false)
   return (
     <div className="App">
-      YESBROKER
+      <button onClick={() => { setShow(true); }}>NIKHIL</button>
+      <Authentication show={show} setShow={setShow} />
     </div>
   );
 }
