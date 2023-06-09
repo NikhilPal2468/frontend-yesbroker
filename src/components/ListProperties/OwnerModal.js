04377594a90248757f7a60652545e82fe5cd0127
Aryan <27aryanraj@gmail.com>
2023-06-09 21:25:56 +0530

    owner modal

--- src/components/ListProperties/OwnerModal.js
+++ src/components/ListProperties/OwnerModal.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Container, Modal } from "react-bootstrap";
+import styles from "./styles.module.css";
 
-function OwnerModal() {
-  return <div>OwnerModal</div>;
+function OwnerModal({ showOnerContacted, setShowOwnerContacted }) {
+  const handleModalClose = () => {
+    setShowOwnerContacted(false);
+  };
+  return (
+    <Modal
+      show={showOnerContacted}
+      onHide={handleModalClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      className={`${styles.modal}`}
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="text-center"></Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body className="show-grid">
+        <Container className="text-center d-flex justify-content-center flex-column">
+          <div className="p-4"></div>
+          <div className="m-4"></div>
+        </Container>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default OwnerModal;
